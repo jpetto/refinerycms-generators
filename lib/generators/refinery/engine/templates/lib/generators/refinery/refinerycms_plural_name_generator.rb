660d527b812fe16a6db5f210c1c9c7dda888a7fd
Fadhli Rahim <fadhlirahim@gmail.com>
2011-07-14 17:59:44 +0800

    Following recent changes for consistent namespacing on generators files

---
+++ lib/generators/refinery/engine/templates/lib/generators/refinery/refinerycms_plural_name_generator.rb
@@ -0,0 +1,8 @@
+module ::Refinery
+  class Refinerycms<%= class_name.pluralize %> < Refinery::Generators::EngineInstaller
+
+    source_root File.expand_path('../../../../', __FILE__)
+    engine_name "<%= plural_name %>"
+
+  end
+end
