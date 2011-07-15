fdb23d4ee93e0ba026d70c559fdbf47e44830c11
Philip Arndt <parndt@gmail.com>
2011-07-15 15:14:10 +1200

    Fully quality namespaces

--- lib/generators/refinery/engine/templates/lib/generators/refinery/plural_name_generator.rb
+++ lib/generators/refinery/engine/templates/lib/generators/refinery/plural_name_generator.rb
@@ -1,5 +1,5 @@
 module ::Refinery
-  class <%= class_name.pluralize %>Generator < Refinery::Generators::EngineInstaller
+  class <%= class_name.pluralize %>Generator < ::Refinery::Generators::EngineInstaller
 
     source_root File.expand_path('../../../../', __FILE__)
     engine_name "<%= plural_name %>"
