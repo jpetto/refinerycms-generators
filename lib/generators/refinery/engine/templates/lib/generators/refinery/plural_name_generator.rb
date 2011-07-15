e9365986bdc2ce78e3cfe292a607e2974ad9a783
Fadhli Rahim <fadhlirahim@gmail.com>
2011-07-15 11:01:47 +0800

    Remove Refinerycms name from generator class name. Maintaining consistency for generated genrator files

--- lib/generators/refinery/engine/templates/lib/generators/refinery/plural_name_generator.rb
+++ lib/generators/refinery/engine/templates/lib/generators/refinery/plural_name_generator.rb
@@ -1,5 +1,5 @@
 module ::Refinery
-  class Refinerycms<%= class_name.pluralize %> < Refinery::Generators::EngineInstaller
+  class <%= class_name.pluralize %> < Refinery::Generators::EngineInstaller
 
     source_root File.expand_path('../../../../', __FILE__)
     engine_name "<%= plural_name %>"
